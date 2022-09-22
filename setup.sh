25c780bd39a942e8c9cb6be11843e68badd41cce
kevinfd <kevinfd@umich.edu>
2022-09-22 15:10:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,1 @@
 git clone https://github.com/CompVis/stable-diffusion.git
-
-# python3 -m venv venv
-
-# source venv/bin/activate
-
-# python3 -m pip install --upgrade pip
-
-# pip install -r requirements.txt 
